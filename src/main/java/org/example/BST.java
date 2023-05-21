33c5ddd6958f3e0aab57d8ee3c908fb306eca8ef
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-05-21 15:48:55 +0600

    put method

--- src/main/java/org/example/BST.java
+++ src/main/java/org/example/BST.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class BST<K extends Comparable<K>, V> {
     private Node root;
+    private int size;
     private class Node{
         private K key;
         private V value;
@@ -12,7 +13,23 @@ public class BST<K extends Comparable<K>, V> {
             }
         }
         public void put(K key, V value){
+            root = put(root, key, value);
+        }
+        private Node put(Node node, K key, V value) {
+            if (node == null) {
+                size++;
+                return new Node(key, value);
+            }
 
+            int cmp = key.compareTo(node.key);
+            if (cmp < 0)
+                node.left = put(node.left, key, value);
+            else if (cmp > 0)
+                node.right = put(node.right, key, value);
+            else
+                node.value = value;
+
+            return node;
         }
         public V get(K key) {
 
