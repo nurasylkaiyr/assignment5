e6cb6e692499db08902dd83be9e8bd63aa88c6ef
nurasylkaiyr <kair.nurasyl@gmail.com>
2023-05-21 16:14:32 +0600

    NodeData method

--- src/main/java/org/example/BST.java
+++ src/main/java/org/example/BST.java
@@ -14,97 +14,114 @@ public class BST<K extends Comparable<K>, V> {
             this.value = value;
             }
         }
-        public void put(K key, V value){
-            root = put(root, key, value);
+    public void put(K key, V value){
+        root = put(root, key, value);
+    }
+    private Node put(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
         }
-        private Node put(Node node, K key, V value) {
-            if (node == null) {
-                size++;
-                return new Node(key, value);
-            }
 
-            int cmp = key.compareTo(node.key);
-            if (cmp < 0)
-                node.left = put(node.left, key, value);
-            else if (cmp > 0)
-                node.right = put(node.right, key, value);
-            else
-                node.value = value;
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, value);
+        else if (cmp > 0)
+            node.right = put(node.right, key, value);
+        else
+            node.value = value;
 
+        return node;
+    }
+
+    public V get(K key) {
+        Node node = get(root, key);
+        return node != null ? node.value : null;
+    }
+    private Node get(Node node, K key) {
+        if (node == null)
+            return null;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            return get(node.left, key);
+        else if (cmp > 0)
+            return get(node.right, key);
+        else
             return node;
-        }
+    }
 
-        public V get(K key) {
-            Node node = get(root, key);
-            return node != null ? node.value : null;
-        }
-        private Node get(Node node, K key) {
-            if (node == null)
-                return null;
+    public void delete(K key){
+        root = delete(root, key);
+    }
+    private Node delete(Node node, K key) {
+        if (node == null)
+            return null;
 
-            int cmp = key.compareTo(node.key);
-            if (cmp < 0)
-                return get(node.left, key);
-            else if (cmp > 0)
-                return get(node.right, key);
-            else
-                return node;
-        }
-
-        public void delete(K key){
-            root = delete(root, key);
-        }
-        private Node delete(Node node, K key) {
-            if (node == null)
-                return null;
-
-            int cmp = key.compareTo(node.key);
-            if (cmp < 0)
-                node.left = delete(node.left, key);
-            else if (cmp > 0)
-                node.right = delete(node.right, key);
-            else {
-                if (node.left == null)
-                    return node.right;
-                else if (node.right == null)
-                    return node.left;
-                else {
-                    Node minNode = findMin(node.right);
-                    node.key = minNode.key;
-                    node.value = minNode.value;
-                    node.right = deleteMin(node.right);
-                }
-            }
-
-            return node;
-        }
-
-        private Node findMin(Node node) {
-            if (node.left == null)
-                return node;
-            return findMin(node.left);
-        }
-        private Node deleteMin(Node node) {
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = delete(node.left, key);
+        else if (cmp > 0)
+            node.right = delete(node.right, key);
+        else {
             if (node.left == null)
                 return node.right;
-            node.left = deleteMin(node.left);
+            else if (node.right == null)
+                return node.left;
+            else {
+                Node minNode = findMin(node.right);
+                node.key = minNode.key;
+                node.value = minNode.value;
+                node.right = deleteMin(node.right);
+            }
+        }
+
+        return node;
+    }
+
+    private Node findMin(Node node) {
+        if (node.left == null)
             return node;
+        return findMin(node.left);
+    }
+    private Node deleteMin(Node node) {
+        if (node.left == null)
+            return node.right;
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
+    public int size() {
+        return size;
+    }
+    public Iterable<K> iterator(){
+        List<K> keys = new ArrayList<>();
+        inOrderTraversal(root, keys);
+        return keys;
+    }
+    private void inOrderTraversal(Node node, List<K> keys) {
+        if (node == null)
+            return;
+
+        inOrderTraversal(node.left, keys);
+        keys.add(node.key);
+        inOrderTraversal(node.right, keys);
+    }
+    public static class NodeData<K, V> {
+        private K key;
+        private V value;
+
+        public NodeData(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
 
-        public int size() {
-            return size;
+        public K getKey() {
+            return key;
         }
-        public Iterable<K> iterator(){
-            List<K> keys = new ArrayList<>();
-            inOrderTraversal(root, keys);
-            return keys;
-        }
-        private void inOrderTraversal(Node node, List<K> keys) {
-            if (node == null)
-                return;
 
-            inOrderTraversal(node.left, keys);
-            keys.add(node.key);
-            inOrderTraversal(node.right, keys);
+        public V getValue() {
+            return value;
         }
+    }
 }
